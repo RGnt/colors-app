4ea413f37b5adeac397fe954dfc1359ee3a5054b
Riku-Juhani Niemela <rjniem@gmail.com>
2019-08-19 18:35:38 +0300

    Colors from the pallet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Palette from './Palette/Palette';
-import seedColors from './seedColors';
+import seedColors from './utils/seedColors';
+import { generatePalette } from './utils/colorHelpers';
 
 const App = () => {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div>
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 };
--- src/Palette/Palette.js
+++ src/Palette/Palette.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ColorBox from '../ColorBox/ColorBox';
 import './Palette.scss';
-const Palette = props => {
-  const colorBoxes = props.colors.map(color => (
-    <ColorBox background={color.color} name={color.name} />
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
+const Palette = ({ palette }) => {
+  //const []
+  const colorBoxes = palette.colors[500].map(color => (
+    <ColorBox key={color.id} background={color.hex} name={color.name} />
   ));
   return (
     <div className="Palette">
