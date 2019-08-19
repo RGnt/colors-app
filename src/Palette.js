aed01ef271cb3798dddc63497ab0b56f6d2ea881
Riku-Juhani Niemela <rjniem@gmail.com>
2019-08-19 04:39:52 +0300

    Palette component start

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar here */}
+        <div className="Palette-colors" />
+        {/* footer eventually */}
+      </div>
+    );
+  }
+}
