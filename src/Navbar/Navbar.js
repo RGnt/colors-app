47deee12cf36d44f3dce1f456ef3136b072a2244
Riku-Juhani Niemela <rjniem@gmail.com>
2019-08-19 23:41:28 +0300

    Snackbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from '@material-ui/core/Select';
 import { MenuItem } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import { Snackbar } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css'
+import 'rc-slider/assets/index.css';
 import './Navbar.scss';
+import { format } from 'upath';
 
-const Navbar = ({ level, changeLevel, handleChange, currentFormat }) => {
+const Navbar = ({ level, changeLevel, handleChange, currentFormat, snackBarOpen, snackBarHandler }) => {
+
     return (
         <header className="Navbar">
             <div className="logo">
-                <a href="">reactcolorpicker</a>
+                <a href="#">reactcolorpicker</a>
             </div>
             <div className="slider-container">
                 <span>Level: {level}</span>
@@ -29,6 +34,32 @@ const Navbar = ({ level, changeLevel, handleChange, currentFormat }) => {
                     <MenuItem value="rgba">RGBA - rgba(255,255,255,0.1)</MenuItem>
                 </Select>
             </div>
+            <Snackbar
+                anchorOrigin={
+                    {
+                        vertical: "bottom",
+                        horizontal: "left"
+                    }}
+                open={snackBarOpen}
+                autoHideDuration={3000}
+                message={
+                    <span id="message-id">
+                        Format Changed To {currentFormat.toUpperCase()}
+                    </span>}
+                ContentProps={{
+                    "aria-describedby": "message-id"
+                }}
+                onClose={() => snackBarHandler(false)}
+                action={[
+                    <IconButton
+                        onClick={() => snackBarHandler(false)}
+                        color='inherit'
+                        key="close"
+                        aria-label="close">
+                        <CloseIcon />
+                    </IconButton>
+                ]}
+            />
         </header>
     );
 };
--- src/Palette/Palette.js
+++ src/Palette/Palette.js
@@ -8,21 +8,38 @@ import './Palette.scss';
 const Palette = ({ palette }) => {
   const [level, setLevel] = useState(500);
   const [format, setFormat] = useState('hex');
+  const [snackBarOpen, setSnackBarStatus] = useState(false);
 
   const changeFormat = (e) => {
     setFormat(e.target.value);
+    setSnackBarStatus(true);
+    setTimeout(() => setSnackBarStatus(false), 2500);
   }
   const changeLevel = (newLevel) => {
     setLevel(newLevel);
   }
+  const snackBarHandler = (val) => {
+    setSnackBarStatus(val)
+  }
 
   const colorBoxes = palette.colors[level].map(color => (
-    <ColorBox key={color.id} background={color[format]} name={color.name} copyFormat={format} />
+    <ColorBox
+      key={color.id}
+      background={color[format]}
+      name={color.name}
+      copyFormat={format} />
   ));
 
   return (
     <div className="Palette">
-      <Navbar level={level} changeLevel={changeLevel} handleChange={changeFormat} currentFormat={format} />
+      <Navbar
+        level={level}
+        changeLevel={changeLevel}
+        handleChange={changeFormat}
+        currentFormat={format}
+        snackBarOpen={snackBarOpen}
+        snackBarHandler={snackBarHandler}
+      />
       {/* Navbar here */}
       <div className="Palette-colors">{colorBoxes}</div>
       {/* footer eventually */}
