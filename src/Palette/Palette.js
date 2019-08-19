b9b7e2844857eb2c11ded28874879520e3da0c22
Riku-Juhani Niemela <rjniem@gmail.com>
2019-08-19 19:15:47 +0300

    Styled the levels slider

--- src/Palette/Palette.js
+++ src/Palette/Palette.js
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
 import ColorBox from '../ColorBox/ColorBox';
-import './Palette.scss';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css'
+import './Palette.scss';
 
 //TODO: Look for better slider solution! Current one uses unsafe lifeCycleMethods!
 
 const Palette = ({ palette }) => {
   const [level, setLevel] = useState(500);
 
+  const changeLevel = (newLevel) => {
+    setLevel(newLevel);
+  }
+
   const colorBoxes = palette.colors[level].map(color => (
     <ColorBox key={color.id} background={color.hex} name={color.name} />
   ));
 
-  const changeLevel = (newLevel) => {
-    setLevel(newLevel);
-  }
   return (
     <div className="Palette">
-      <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel} />
+      <div className='slider'>
+        <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel} />
+      </div>
       {/* Navbar here */}
       <div className="Palette-colors">{colorBoxes}</div>
       {/* footer eventually */}
